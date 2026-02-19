07dd1a5421ede3a1d3b21a4e3c7874498ad1a88c
shanwei0949 <shanwei0949@163.com>
2026-02-19 22:32:39 +0800

    add counter contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter{
+
+    uint256 public count;
+
+    function get() public view returns (uint256){
+        return count;
+    }
+
+    function add(uint256 x)public {
+        count += x;
+    }
+}
